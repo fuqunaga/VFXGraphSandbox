c22ca0ae2e21560b3efd0058fb13669ec1e10970
fuqunaga <fukhid@gmail.com>
2019-12-22 14:49:07 +0900

    update

---
+++ Assets/RuntimePointCache/Gemerator/MeshParticleGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[ExecuteAlways]
+[RequireComponent(typeof(Collider))]
+public class MeshParticleGenerator : MonoBehaviour
+{
+    public MeshParticle meshParticlePrefab;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var renderer = other.GetComponent<Renderer>();
+        var mp = Instantiate(meshParticlePrefab);
+
+        mp.model = other.gameObject;
+        mp.startEffect = true;
+
+        StartCoroutine(DestroyDelay(mp.gameObject, mp.effectDiableDelay + 1f));
+
+    }
+
+    IEnumerator DestroyDelay(GameObject target, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        Destroy(target);
+    }
+}
---
+++ Assets/RuntimePointCache/Gemerator/MeshParticleGeneratorClick.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshParticleGeneratorClick : MonoBehaviour
+{
+    public MeshParticle meshParticlePrefab;
+    public float interval;
+    float lastClickTime;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && Time.time - lastClickTime > interval)
+        {
+            lastClickTime = Time.time;
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out var hit))
+            {
+                var other = hit.collider;
+                var renderer = other.GetComponent<Renderer>();
+                var mp = Instantiate(meshParticlePrefab);
+
+                mp.model = other.gameObject;
+                mp.startEffect = true;
+
+                //StartCoroutine(DestroyDelay(mp.gameObject, mp.effectDiableDelay + 1f));
+            }
+
+        }
+    }
+}
