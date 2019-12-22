9c2beb765f9417798c7c5c34d4b0e59924e70d08
fuqunaga <fukhid@gmail.com>
2019-12-22 17:21:59 +0900

    add VAT extension

---
+++ Assets/VATExtension/SimpleVAT/Editor/SimpleVATBaker.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+
+public class SimpleVATBaker : EditorWindow
+{
+    [MenuItem("Window/SimpleVATBaker")]
+    public static void ShowWindow()
+    {
+        GetWindow<SimpleVATBaker>();
+    }
+
+
+    public GameObject targetObj;
+    public SkinnedMeshRenderer skinnedMeshRenderer;
+    public float fps = 60f;
+
+
+    private void OnGUI()
+    {
+        targetObj = (GameObject)EditorGUILayout.ObjectField(targetObj, typeof(GameObject), true);
+        skinnedMeshRenderer = (SkinnedMeshRenderer)EditorGUILayout.ObjectField(skinnedMeshRenderer, typeof(SkinnedMeshRenderer), true);
+
+        if (GUILayout.Button("Bake!"))
+        {
+            Bake();
+        }
+    }
+
+    void Bake()
+    {
+        var animation = targetObj.GetComponent<Animation>();
+        var clip = animation.clip;
+        var length = clip.length;
+        var timeStepCount = Mathf.FloorToInt(length * fps);
+
+        var vertexCount = skinnedMeshRenderer.sharedMesh.vertexCount;
+        /*
+        var totalCount = vertexCount * timeStepCount;
+
+        
+        var width = Mathf.Min(totalCount, 2048);
+        var height = Mathf.CeilToInt(totalCount / width);
+        */
+
+
+        var tex = new Texture2D(vertexCount, timeStepCount, TextureFormat.RGBAFloat, false);
+        tex.filterMode = FilterMode.Point;
+        tex.wrapMode = TextureWrapMode.Clamp;
+
+        var colors = new List<Color>(vertexCount * timeStepCount);
+
+
+        var mesh = new Mesh();
+        var vertices = new List<Vector3>(vertexCount);
+        for (var i = 0; i < timeStepCount; ++i)
+        {
+            clip.SampleAnimation(targetObj, i / fps);
+
+            skinnedMeshRenderer.BakeMesh(mesh);
+
+            mesh.GetVertices(vertices);
+
+            colors.AddRange(vertices.Select(v => new Color(v.x, v.y, v.z)));
+        }
+
+        tex.SetPixels(colors.ToArray());
+
+        AssetDatabase.CreateAsset(tex, "Assets/vat.asset");
+
+#if false
+        var data = tex.EncodeToPNG();
+
+        string filePath = EditorUtility.SaveFilePanel("Save Texture", Application.dataPath, "vat.png", "png");
+        if (!string.IsNullOrEmpty(filePath))
+        {
+            File.WriteAllBytes(filePath, data);
+        }
+#endif
+    }
+}
---
+++ Assets/VATExtension/SimpleVAT/SimpleVAT.shader
@@ -0,0 +1,67 @@
+﻿Shader "Custom/UnlitSimpleVAT"
+{
+    Properties
+    {
+        _MainTex ("MainTex", 2D) = "white" {}
+        _VATex ("VATex", 2D) = "white" {}
+        //_AnimTime("AnimTime", float) = 0.0
+        _AnimFPS("AnimFPX", float) = 60.0
+    }
+    SubShader
+    {
+        Tags { "RenderQueue"="Qpaque" "RenderType"="Opaque" }
+        LOD 100
+        ZTest Always
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+
+            Texture2D<float4> _VATex;
+            float _AnimTime;
+            float _AnimFPS;
+
+            v2f vert (appdata v, uint vId : SV_VertexID)
+            {
+                v2f o;
+
+                uint timeStep = _Time.y * _AnimFPS;
+
+                uint width, height;
+                _VATex.GetDimensions(width, height);
+
+                float3 pos = _VATex[uint2(vId, timeStep % height)];
+
+                o.vertex = UnityObjectToClipPos(pos);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                fixed4 col = tex2D(_MainTex, i.uv);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
